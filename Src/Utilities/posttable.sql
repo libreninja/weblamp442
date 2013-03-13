30257df12abeb1895e0aa658877bfcd53240e34b
Josh Benner <sjbenner@gmail.com>
2013-03-13 13:23:03 -0700

    added title to post db table

--- Src/Utilities/posttable.sql
+++ Src/Utilities/posttable.sql
@@ -1,7 +1,9 @@
 use weblamp442;
+DROP TABLE Post;
 
 CREATE TABLE Post (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
     post TEXT,
     author_id INT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES User(id)
